0057e550a9e2d4dc8a958ab6e93c47ce2fe30553
BbbGoat <oomi9421@gmail.com>
2023-09-25 17:23:57 +0900

    ss

---
+++ fresh/app/cart/data.js
@@ -0,0 +1,4 @@
+let age = 20;
+let name = 'kim';
+
+export { age }
--- fresh/app/cart/page.js
+++ fresh/app/cart/page.js
@@ -1,19 +1,23 @@
 import Link from "next/link"
+import { age, name } from './data'
 
 export default function Cart() {
   return (
     <div>
       <h4 className="title">Cart</h4>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
+      <CartItem />
+      <CartItem />
+      <CartItem />
     </div>
   )
-} 
+}
+
+function CartItem() {
+  return(
+    <div className="cart-item">
+        <p>상품명 {age}</p>
+        <p>$40</p>
+        <p>1개</p>
+    </div>
+  )
+}
