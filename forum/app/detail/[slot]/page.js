9f46751485b686a659a51403990a0b8c674e1fdb
BbbGoat <oomi9421@gmail.com>
2023-09-26 16:48:03 +0900

    Update page.js

--- forum/app/detail/[slot]/page.js
+++ forum/app/detail/[slot]/page.js
@@ -1,17 +1,18 @@
 import { connectDB } from "@/app/util/database"
+import { ObjectId } from "mongodb";
 
-export default async function Detail() {
+export default async function Detail(props) {
     const client = await connectDB;
     const db = client.db('forum')
-    let result = await db.collection('post').findOne({ title : '제목3' })
-    console.log(result)
-    
+    let result = await db.collection('post').findOne({ _id: new ObjectId(props.params.slot) })
+    console.log(props.params.slot)
+
     return(
         <>
             <div>
                 <h4>상세페이지</h4>
-                <h4>글제목</h4>
-                <p>글내용</p>
+                <h4>{result.title}</h4>
+                <p>{result.content}</p>
             </div>
         </>
     )
