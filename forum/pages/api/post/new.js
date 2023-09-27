41a235d0a912fb80b84a9e6696ecf6bdc1a0280d
BbbGoat <oomi9421@gmail.com>
2023-09-27 20:27:12 +0900

    Update new.js

--- forum/pages/api/post/new.js
+++ forum/pages/api/post/new.js
@@ -5,6 +5,11 @@ export default async function handler(요청, 응답) {
     if ( 요청.method == 'POST' ) {
         const client = await connectDB;
         const db = client.db('forum')
+
+        if (요청.body.title == '') {
+            return 응답.status(500).json('제목 써야합니다')
+        }
+        
         let result = await db.collection('post').insertOne(요청.body)
          
         return 응답.redirect(302, '/list')
