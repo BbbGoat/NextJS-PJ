f0e4184e5ae4d0b39c3ffa5bba5204677ebcc724
BbbGoat <oomi9421@gmail.com>
2023-09-30 18:37:30 +0900

    Update DetailLink.js

--- forum/app/list/DetailLink.js
+++ forum/app/list/DetailLink.js
@@ -5,6 +5,6 @@ import { useRouter } from "next/navigation"
 export default function DetailLink() {
     let router = useRouter()
     return(
-        <button onClick={()=>{ router.back() }}>버튼</button>
+        <button onClick={()=>{ router.back() }}>뒤로가기</button>
     )
 }
