a1a741ebe00aa5db77856ec28fe7ba00ce341d8d
BbbGoat <oomi9421@gmail.com>
2023-10-29 20:54:59 +0900

    ss

--- react-shop-app/src/app/(order)/order-history/OrderHistoryClient.jsx
+++ react-shop-app/src/app/(order)/order-history/OrderHistoryClient.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import styles from './OrderHistory.module.scss'
+import useFetchCollection from '@/hooks/useFetchCollection'
+import { useDispatch, useSelector } from 'react-redux';
+import { STORE_ORDERS, selectOrderHistory } from '@/redux/slice/orderSlice';
+import { selectUserId } from '@/redux/slice/authSlice';
 
 const OrderHistoryClient = () => {
+
+    // 주문 데이터 받아오기
+    const {data, isLoading} = useFetchCollection('orders');
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        dispatch(STORE_ORDERS(data));
+
+    }, [dispatch, data]);
+
+    const orders = useSelector(selectOrderHistory);
+    const userId = useSelector(selectUserId);
+    
   return (
     <div>OrderHistoryClient</div>
   )
