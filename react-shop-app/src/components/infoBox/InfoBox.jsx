f06c9b867b4675ea287a0c7d8c226bd13e50de2c
BbbGoat <oomi9421@gmail.com>
2023-10-31 20:21:55 +0900

    ss

--- react-shop-app/src/components/infoBox/InfoBox.jsx
+++ react-shop-app/src/components/infoBox/InfoBox.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import styles from './InfoBox.module.scss'
 
-const InfoBox = () => {
+const InfoBox = ({cardClass, title, count, icon}) => {
   return (
-    <div>InfoBox</div>
+    <div className={styles.infoBox}>
+      <div className={cardClass}>
+        <h4>{title}</h4>
+        <span>
+          <h3>{count}</h3>
+          {icon}
+        </span>
+      </div>
+    </div>
   )
 }
 
--- react-shop-app/src/redux/slice/orderSlice.js
+++ react-shop-app/src/redux/slice/orderSlice.js
@@ -11,11 +11,24 @@ const orderSlice = createSlice({
     reducers: {
         STORE_ORDERS(state, action) {
             state.orderHistory = action.payload;
+        },
+        CALCULATE_TOTAL_ORDER_AMOUNT(state, action) {
+            const array = [];
+            state.orderHistory.map((item)=>{
+                const {orderAmount} = item;
+                return array.push(orderAmount);
+            })
+            const totalAmount = array.reduce((a,b)=>{
+                return a + b
+            }, 0);
+
+            state.totalOrderAmount = totalAmount
         }
     }
 })
 
-export const { STORE_ORDERS } = orderSlice.actions;
+export const { STORE_ORDERS, CALCULATE_TOTAL_ORDER_AMOUNT } = orderSlice.actions;
 export const selectOrderHistory = (state) => state.orders.orderHistory;
+export const selectTotalOrderAmount = (state) => state.orders.totalOrderAmount;
 
 export default orderSlice.reducer;
